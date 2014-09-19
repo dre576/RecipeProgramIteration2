2e53c0d8ea1e5e139a4d0206edd899e3aa4f56ce
NickelSilver <maysmith89@gmx.com>
2014-09-18 19:36:13 -0600

    Updated mainFrame Class
    Updated mainFrame.java with more descriptive names and event handlers.

--- mainFrame.java
+++ mainFrame.java
@@ -27,12 +27,16 @@ import java.awt.event.ActionEvent;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class mainFrame extends JFrame
 {
-	private JTextField textField;
-	private JTable table;
-	private JTextField textField_1;
+	private JTextField tagsField;
+	private JTable recipeTable;
+	private JTextField searchBar;
 
 	/**
 	 * Launch the application.
@@ -70,11 +74,11 @@ public class mainFrame extends JFrame
 		getContentPane().add(panel);
 		panel.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Recipes");
-		lblNewLabel.setBounds(10, 11, 241, 54);
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Sylfaen", Font.PLAIN, 50));
-		panel.add(lblNewLabel);
+		JLabel lblRecipes = new JLabel("Recipes");
+		lblRecipes.setBounds(10, 11, 241, 54);
+		lblRecipes.setHorizontalAlignment(SwingConstants.CENTER);
+		lblRecipes.setFont(new Font("Sylfaen", Font.PLAIN, 50));
+		panel.add(lblRecipes);
 		
 		JLabel lblCategory = new JLabel("Category");
 		lblCategory.setBounds(10, 76, 57, 14);
@@ -84,49 +88,79 @@ public class mainFrame extends JFrame
 		lblTags.setBounds(10, 101, 46, 14);
 		panel.add(lblTags);
 		
-		textField = new JTextField();
-		textField.setBounds(69, 98, 171, 20);
-		panel.add(textField);
-		textField.setColumns(10);
+		tagsField = new JTextField();
+		tagsField.setBounds(69, 98, 171, 20);
+		panel.add(tagsField);
+		tagsField.setColumns(10);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(69, 73, 171, 20);
-		panel.add(comboBox);
+		JComboBox categoryDropdown = new JComboBox();
+		categoryDropdown.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+			}
+		});
+		categoryDropdown.setBounds(69, 73, 171, 20);
+		panel.add(categoryDropdown);
 		
 		String[] columnName = {"Title"};
 		Object[][] data = {{"Pancakes"},{"Eggs and Bacon"},{"Banana Bread"} };
 		
 		JButton btnAdd = new JButton("Add");
+		btnAdd.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
 		btnAdd.setBounds(10, 512, 70, 23);
 		panel.add(btnAdd);
 		
 		JButton btnEdit = new JButton("Edit");
+		btnEdit.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
 		btnEdit.setBounds(90, 512, 70, 23);
 		panel.add(btnEdit);
 		
 		JButton btnDelete = new JButton("Delete");
+		btnDelete.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
 		btnDelete.setBounds(170, 512, 70, 23);
 		panel.add(btnDelete);
 		
-		JEditorPane editorPane = new JEditorPane();
-		editorPane.setBounds(250, 11, 584, 490);
-		panel.add(editorPane);
+		JEditorPane textArea = new JEditorPane();
+		textArea.setBounds(250, 11, 584, 490);
+		panel.add(textArea);
 		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 126, 230, 375);
 		panel.add(scrollPane);
 		
-		table = new JTable(data, columnName);
-		table.setFillsViewportHeight(true);
-		scrollPane.setViewportView(table);
+		recipeTable = new JTable(data, columnName);
+		recipeTable.setFillsViewportHeight(true);
+		scrollPane.setViewportView(recipeTable);
 		
 		JButton btnSearch = new JButton("Search");
+		btnSearch.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
 		btnSearch.setBounds(250, 512, 70, 23);
 		panel.add(btnSearch);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(330, 513, 504, 20);
-		panel.add(textField_1);
-		textField_1.setColumns(10);
+		searchBar = new JTextField();
+		searchBar.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+			}
+		});
+		searchBar.setBounds(330, 513, 504, 20);
+		panel.add(searchBar);
+		searchBar.setColumns(10);
 	}
 }
