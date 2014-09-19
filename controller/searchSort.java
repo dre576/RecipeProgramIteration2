82822d39be8a2010406513d3461c91dd88a466ca
dre576 <aj90008@gmail.com>
2014-09-19 15:54:08 -0600

    controller
    got rid of unnecessary classes, put original search sort back up

--- controller/searchSort.java
+++ controller/searchSort.java
@@ -1,19 +1,9 @@
-package controller;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Map;
 import java.util.Set;
-
-import javax.swing.JOptionPane;
-
-import model.Category;
-import model.Ingredient;
-import model.Recipe;
 
 public class searchSort {
 	
@@ -22,8 +12,8 @@ public class searchSort {
 
 		ArrayList<Recipe> foundRecipesArray = new ArrayList<Recipe>();
 		for (Recipe recipe : recipeArray) {
-			if (recipe.getTitle().toLowerCase().contains(searchedString.toLowerCase()) 
-					|| searchedString.toLowerCase().contains(recipe.getTitle().toLowerCase())) {
+			if (recipe.getName().toLowerCase()
+					.contains(searchedString.toLowerCase())) {
 				foundRecipesArray.add(recipe);
 			}
 		}
@@ -37,15 +27,13 @@ public class searchSort {
 
 		ArrayList<Recipe> foundRecipesArray = new ArrayList<Recipe>();
 		for (Recipe recipe : recipeArray) {
-			Iterator it = recipe.getIngredients().entrySet().iterator();
-		    while (it.hasNext()) {
-		        Map.Entry pairs = (Map.Entry)it.next();
-		        if (((Ingredient) pairs.getKey()).getName().toLowerCase().contains(searchedString.toLowerCase())
-		        		|| searchedString.toLowerCase().contains(((Ingredient) pairs.getKey()).getName().toLowerCase())) {
+			for (String ingredient : recipe.getIngredients()) {
+				if (ingredient.toLowerCase().contains(
+						searchedString.toLowerCase())) {
 					foundRecipesArray.add(recipe);
 					break;
 				}
-		    }
+			}
 		}
 
 		return foundRecipesArray;
@@ -57,9 +45,9 @@ public class searchSort {
 
 		ArrayList<Recipe> foundRecipesArray = new ArrayList<Recipe>();
 		for (Recipe recipe : recipeArray) {
-			for (Category category : recipe.getCategories()) {
-				if (category.getName().toLowerCase().contains(searchedString.toLowerCase())
-						|| searchedString.toLowerCase().contains(category.getName().toLowerCase())) {
+			for (String category : recipe.getCategories()) {
+				if (category.toLowerCase().contains(
+						searchedString.toLowerCase())) {
 					foundRecipesArray.add(recipe);
 					break;
 				}
@@ -70,11 +58,11 @@ public class searchSort {
 
 	}
 
-	public static Set<Category> findCategories(ArrayList<Recipe> recipeArray) {
-		Set<Category> categorySet = new HashSet<Category>();
+	public static Set<String> findCategories(ArrayList<Recipe> recipeArray) {
+		Set<String> categorySet = new HashSet<String>();
         for (Recipe r : recipeArray) {
-            for (Category i : r.getCategories()) {
-                categorySet.add(i);
+            for (String i : r.getCategories()) {
+                categorySet.add(i.toLowerCase());
             }
         }
 		return categorySet;
@@ -91,50 +79,12 @@ public class searchSort {
 		Collections.sort(alphabeticalList, new Comparator<Recipe>() {
 			@Override
 			public int compare(Recipe o1, Recipe o2) {
-				return o1.getTitle().toLowerCase()
-						.compareTo(o2.getTitle().toLowerCase());
+				return o1.getName().toLowerCase()
+						.compareTo(o2.getName().toLowerCase());
 			}
 		});
 
 		return alphabeticalList;
-	}
-	
-	public static HashMap<Ingredient, ArrayList<Recipe>> getIngredientsRecipes(ArrayList<Recipe> recipes) {
-		HashMap<Ingredient, ArrayList<Recipe>> ingredientsRecipes = new HashMap<Ingredient, ArrayList<Recipe>>();
-		ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>();
-		for (Recipe recipe : recipes) {
-			Iterator it = recipe.getIngredients().entrySet().iterator();
-		    while (it.hasNext()) {
-		        Map.Entry pairs = (Map.Entry)it.next();
-		        if(ingredientsRecipes.containsKey(pairs.getKey())){
-		        	ingredientsRecipes.get(pairs.getKey()).add(recipe);
-		        	//ingredientsRecipes.put(pairs.getKey(), );
-		        } else {
-		        	ArrayList<Recipe> list = new ArrayList<Recipe>();
-		        	list.add(recipe);
-		        	ingredientsRecipes.put((Ingredient) pairs.getKey(), list);
-		        }
-		    }
-		}
-		return ingredientsRecipes;
-	}
-
-	public static HashMap<Category, ArrayList<Recipe>> getCategoriesRecipes(ArrayList<Recipe> recipes) {
-		HashMap<Category, ArrayList<Recipe>> ingredientsCategories = new HashMap<Category, ArrayList<Recipe>>();
-		ArrayList<Category> categories = new ArrayList<Category>();
-		for (Recipe recipe : recipes) {
-			for (Category category : recipe.getCategories()) {
-				 if(ingredientsCategories.containsKey(category)){
-					 ingredientsCategories.get(category).add(recipe);
-			        } else {
-			        	ArrayList<Recipe> list = new ArrayList<Recipe>();
-			        	list.add(recipe);
-			        	ingredientsCategories.put(category, list);
-			        }
-			}
-		}
-		
-		return ingredientsCategories;
 	}
 
 }
